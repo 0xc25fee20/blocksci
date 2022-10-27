a6332038ded27c134b5eea1f9da357eab096707c
0xc25fee20 <lcycugb@gmail.com>
2022-10-27 17:27:52 +0800

    start

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,7 +18,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            BlockSCI Tutorial - 5min ⏱️
+            BlockSCI 介绍 - 5min ⏱️
           </Link>
         </div>
       </div>
