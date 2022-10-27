95ccc6f7a5ff8edbb4a83ff26734e2a656e74011
0xc25fee20 <lcycugb@gmail.com>
2022-10-27 19:37:14 +0800

    start

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'BlockSCI',
   tagline: 'Crypto is cool',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://blocksci.xyz',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
